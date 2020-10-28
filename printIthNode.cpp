fd3a94279fcea53ecd9c90bd8b34f8e6ffb96c15
Ankit Bhankharia <71999020+cupcake08@users.noreply.github.com>
2020-10-28 19:49:13 +0530

    Create printIthNode.cpp

---
+++ printIthNode.cpp
@@ -0,0 +1,71 @@
+//problem from coding ninjas platform
+#include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newNode = new Node(data);
+		if (head == NULL)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+			tail = newNode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+/*******************************************************************/
+void printIthNode(Node *head, int i)
+{
+    //Write your code here
+    Node *temp=head;
+    int sum=0;
+    while(temp!=NULL)
+    {
+        if(sum==i)
+        {
+          cout<<temp->data;
+          return;
+        }
+        temp=temp->next;
+        sum++;
+    }
+}
+/******************************************************************/
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head = takeinput();
+		int pos;
+		cin >> pos;
+		printIthNode(head, pos);
+		cout << endl;
+	}
+	return 0;
+}
