a8a4254e933762f7cb44b7c6ad9463715858605c
Ankit Bhankharia <71999020+cupcake08@users.noreply.github.com>
2020-10-28 19:57:56 +0530

    Create appendLastNtoFirst.cpp

---
+++ appendLastNtoFirst.cpp
@@ -0,0 +1,40 @@
+//coding ninjas platform
+
+
+int length(Node *head)
+{
+    if(head==NULL)
+    {
+        return 0;
+    }
+    return 1+length(head->next);
+}
+Node *appendLastNToFirst(Node *head, int n)
+{
+    Node *temp=head;
+    int x=length(head)-n;
+    int count=0;
+    Node *a=head;
+    Node *b;
+    if(head==NULL)
+    {
+        return head;
+    }
+    if(n==0)
+    {
+        return head;
+    }
+    while(temp->next!=NULL)
+    {
+      if(count==x-1)
+      {
+        head=temp->next;
+        b=temp;
+      }
+      temp=temp->next;
+      count++;
+    }
+    temp->next=a;
+    b->next=NULL;
+    return head;
+}
